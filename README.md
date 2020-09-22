08bbef42a3b9fca24825781a59711dbc3bf5adb0
John Grando <grandocu@gmail.com>
2020-09-22 16:24:37 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+data location:https://www.ncei.noaa.gov/data/global-summary-of-the-day/archive/
---
+++ TestScript.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 14:48:09 2020
+
+@author: grandocu
+"""
+
+class PySparkClass():
+    
+    def __init__(self):
+        try:
+            self.sc and self.spark
+        except (AttributeError, NameError, UnboundLocalError) as e:
+            print('sc and spark not found: {}'.format(e))
+            import findspark
+            findspark.init()
+            import pyspark
+            conf = pyspark\
+                .SparkConf()
+            self.sc = pyspark.SparkContext(conf=conf)
+            self.spark = pyspark\
+                .sql\
+                .SparkSession(self.sc)\
+                .builder\
+                .appName("testingAppName")\
+                .getOrCreate()
+            return
+import os, sys, random, re
+file_path = os.path.join(os.path.dirname(__file__), "WeatherCSV")
+only_csv_rgx = re.compile(r'.*\.csv$')
+file_list = [f for f in os.listdir(file_path) if os.path.isfile(os.path.join(file_path, f)) and re.match(only_csv_rgx, f)]
+
+random.seed(82828)
+file_list_short = ['hdfs://localhost:9000/WeatherCSV/{}'.format(f) for f in random.sample(file_list, 200)]
+
+myClass = PySparkClass()
+df_load = myClass\
+    .spark\
+    .read\
+    .option("header", "true")\
+    .csv(file_list_short)
+
+print(df_load.first())
